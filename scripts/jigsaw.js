a93d0d5d87e7d56558909b18710fe9fc2f398da3
Vipul Chauhan <vip.chauhan99@gmail.com>
2021-05-14 18:58:59 +0530

    Working Code with bugs

--- scripts/jigsaw.js
+++ scripts/jigsaw.js
@@ -5,13 +5,14 @@ var maindiv;
 var block1_selected;
 var level;
 var imgs_path;
-var solution_shown = false;
+var solution_shown;
 
 
 
-function Block(img_location, current_loc, correct_loc, img) {
+function Block(img_location, correct_loc, i, img) {
 	this.img = img;
 	this.img_location = img_location;
+	this.current_i = i;
 	this.correct_loc= correct_loc;
 	this.selected = false;
 }
@@ -25,10 +26,10 @@ function Board(size, row, column) {
 
 	// path
 	imgs_path = "imgs/level"
-	if(level==1) imgs_path += "1/";
-	else if(level==2) imgs_path += "2/";
-	else imgs_path += "3/";
-	imgs_path += Math.round(Math.random()*1).toString() + "/";
+	if(level==1) imgs_path += "1_";
+	else if(level==2) imgs_path += "2_";
+	else imgs_path += "3_";
+	imgs_path += Math.round(Math.random()*1).toString() + "_";
 
 	// console.log(imgs_path);
 
@@ -44,7 +45,12 @@ function Board(size, row, column) {
 	// insert into array
 	let tmp;
 	for(let i=0; i<size;i++) {
-		tmp = new Block(rand_images[i], i, parseInt(rand_images[i].charAt(14)), 0);
+		tmp = new Block(
+			rand_images[i],
+			parseInt(rand_images[i].charAt(14)),
+			i,
+			null
+		);
 		this.array.push(tmp);
 	}
 
@@ -53,24 +59,27 @@ function Board(size, row, column) {
 
 
 
-function swap_blocks(block1, block2) {
-	var tmp1 = block2.img_location;
-	var tmp2 = block2.img.current_loc;
-	var tmp3 = block2.selected;
-	block2.img_location = block1.img_location;
-	block2.current_loc = block1.current_loc;
-	block2.selected = block1.selected;
-	block1.img_location = tmp1;
-	block1.current_loc = tmp2;
-	block1.selected = tmp3;
-	block1.img.src = block1.img_location;
-	block2.img.src = block2.img_location;
-}
+// function swap_blocks(block1, block2) {
+// 	var tmp1 = block2.img_location;
+// 	var tmp2 = block2.img.current_loc;
+// 	var tmp3 = block2.selected;
+// 	block2.img_location = block1.img_location;
+// 	block2.current_loc = block1.current_loc;
+// 	block2.selected = block1.selected;
+// 	block1.img_location = tmp1;
+// 	block1.current_loc = tmp2;
+// 	block1.selected = tmp3;
+// 	block1.img.src = block1.img_location;
+// 	block2.img.src = block2.img_location;
+// }
 
 function randomize(array) {
 	arr = array.slice();
 	let size = array.length;
 	let curr_i = size-1;
+	// if()
+
+
 	while(curr_i != 0) {
 		let rand_i = Math.floor(Math.random()*curr_i);
 		curr_i-=1;
@@ -87,6 +96,11 @@ function init(level) {
 		remove_blocks();
 	}
 
+	if(solution_shown) {
+		remove_solution();
+		solution_shown = false;
+	}
+
 	if(level==1)
 		board = new Board(2, 1, 2);
 	else if(level==2) 
@@ -96,9 +110,8 @@ function init(level) {
 	
 	// insert images
 	maindiv = document.querySelector("#block");
-	var tmp;
 	for(let i=0; i<board.array.length; i++) {
-		tmp = document.createElement("img");
+		let tmp = document.createElement("img");
 		tmp.src = board.array[i].img_location;
 		tmp.width = 200;
 		tmp.classList = "imgs";
@@ -111,7 +124,12 @@ function init(level) {
 	// initialise events for board
 	for(let i=0; i<board.array.length; i++) {
 		board.array[i].img.addEventListener("click", e => {
-			block_click(i, e);
+			for(let j=0; j<board.array.length;j++) {
+				if(board.array[j].current_i == i){
+					block_click(j, e);
+					break;
+				}
+			}
 		});
 	}
 }
@@ -136,17 +154,9 @@ function block_select(i, e) {
 	board.array[i].img.style.opacity = 0.5;
 	board.array[i].selected = true;
 	board.array[i].img.style.zIndex = 9999;
-
-	// events
-	console.log("move block");
-
 	board.array[i].img.style.position = "absolute";
-	// let box_dim = board.array[i].img.getBoundingClientRect();
-	// board.array
-
 	board.array[i].img.addEventListener("mousemove", block_move);
 }
-
 
 function block_deselect(i) {
 	board.array[i].selected = false;
@@ -155,15 +165,13 @@ function block_deselect(i) {
 	board.array[i].img.removeEventListener("mousemove", block_move);
 }
 
-
-
 function block_move(event) {
 
 	let mouse_x = event.pageX;
 	let mouse_y = event.pageY;
 
-	let box_top = mouse_y - event.target.height*1;
-	let box_left = mouse_x - event.target.width*1;
+	let box_top = mouse_y - event.target.height*1.25;
+	let box_left = mouse_x - event.target.width*1.5;
 
 	let dim = document.querySelector("#block").getBoundingClientRect();
 
@@ -178,8 +186,6 @@ function block_move(event) {
 	}
 }
 
-
-
 function check_board_correct() {
 	let isCorrect = true;
 	// udate current_loc
@@ -193,8 +199,6 @@ function check_board_correct() {
 	return isCorrect;
 }
 
-
-
 function update_location() {
 	board.array.sort((e1, e2)=> {
 		let e1_rect = e1.img.getBoundingClientRect();
@@ -206,32 +210,34 @@ function update_location() {
 	});
 }
 
-
-
 function add_solution() {
+	console.log("win");
 	let maindiv = document.querySelector("#block");
 	let dim = [maindiv.getBoundingClientRect()['width'], maindiv.getBoundingClientRect()['height']];
+
+	maindiv.style.display = "flex";
+	maindiv.style.flexDirection = "row";
+	maindiv.style.justifyContent = "space-around";
+
 	let tmp = document.createElement("img");
 	tmp.src = imgs_path + "image.jpg";
 	tmp.classList = "win-image";
 	tmp.style.position = "relative";
-	tmp.style.height = dim[1] + "px";
 	tmp.style.width = dim[0] + "px";
+	if(level==1)
+		tmp.style.height = dim[0]/2 + "px";
+	if(level==2)
+		tmp.style.height = dim[0]/3 + "px";
+	if(level==3)
+		tmp.style.height = (dim[0]/5)*2 + "px";	
+	// tmp.style.width = "auto";
 	maindiv.appendChild(tmp);
-	solution_shown = true;
 }
-
-
-
 
 function remove_solution() {
-	if(solution_shown) {
-		document.querySelector(".win-image").remove();
-		solution_shown = false;
-	}
+	document.querySelector(".win-image").remove();
+	document.querySelector("#block").style.display = "block";
 }
-
-
 
 function remove_blocks() {
 	for(let i=0; i<board.array.length; i++) {
@@ -239,6 +245,8 @@ function remove_blocks() {
 	}
 	board = null;
 }
+
+
 
 
 
@@ -254,30 +262,25 @@ function start(l) {
 
 // events
 document.querySelector("#level1").addEventListener("click", e => {
-	if(board!= null)
-		remove_blocks();
 	start(1);
 });
 
 document.querySelector("#level2").addEventListener("click", e => {
-	if(board!= null)
-		remove_blocks();
-	remove_solution();
 	start(2);
 });
 
 document.querySelector("#level3").addEventListener("click", e => {
-	if(board!= null)
-		remove_blocks();
-	remove_solution();
 	start(3);
 });
 
 document.querySelector("#check").addEventListener("click", e => {
-	update_location();
-	if(check_board_correct() && !solution_shown) {
-		remove_blocks();
-		add_solution();
+	if(board!=null) {
+		update_location();
+		if(check_board_correct() && !solution_shown) {
+			remove_blocks();
+			add_solution();
+			solution_shown = true;
+		}
 	}
 })
 
