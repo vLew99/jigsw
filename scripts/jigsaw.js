173e43ec8b4476dd9d97325a0e2a25245d3cc9e7
Vipul Chauhan <vip.chauhan99@gmail.com>
2021-05-26 18:48:00 +0530

    Changed where the images are entered in the block

--- scripts/jigsaw.js
+++ scripts/jigsaw.js
@@ -1,23 +1,25 @@
 // CONSTANTS
 
 var board;
-var maindiv;
-var block1_selected;
+const maindiv = document.querySelector("#block");
+var block_selected = false;
 var level;
 var imgs_path;
 var solution_shown;
 
-
-
-function Block(img_location, correct_loc, i, img) {
+function Block(file_location, correct_loc, current_loc, img) {
 	this.img = img;
-	this.img_location = img_location;
-	this.current_i = i;
+	this.file_location = file_location;
+	this.current_loc = current_loc;
 	this.correct_loc= correct_loc;
 	this.selected = false;
 }
 
-
+function file_location_string(level) {
+	let file_location = "imgs/level" + level.toString() + "_";
+	file_location += Math.round(Math.random()*1).toString() + "_";
+	return file_location;
+}
 
 function Board(size, row, column) {
 	this.array = new Array();
@@ -25,61 +27,39 @@ function Board(size, row, column) {
 	this.running = true;
 
 	// path
-	imgs_path = "imgs/level"
-	if(level==1) imgs_path += "1_";
-	else if(level==2) imgs_path += "2_";
-	else imgs_path += "3_";
-	imgs_path += Math.round(Math.random()*1).toString() + "_";
+	imgs_path = file_location_string(level);
 
-	// console.log(imgs_path);
-
-	// images path and randomize
 	images = [];
 	for(let i=0; i < size; i++)
 		images.push(imgs_path + i.toString() + ".png");
 	rand_images = randomize(images);
 
-	// console.log(images);
-	// console.log(rand_images);
-
 	// insert into array
-	let tmp;
+	let tmp_block;
+	let tmp_img;
 	for(let i=0; i<size;i++) {
-		tmp = new Block(
-			rand_images[i],
-			parseInt(rand_images[i].charAt(14)),
-			i,
-			null
-		);
-		this.array.push(tmp);
-	}
+		// block
+		tmp_block = new Block(rand_images[i],parseInt(rand_images[i].charAt(14)),i,null);
+		
+		// img
+		tmp_img = document.createElement("img");
+		tmp_img.src = tmp_block.file_location; // getting file location from block
+		tmp_img.width = 200;
+		tmp_img.classList = "imgs";
+		tmp_img.style.position = "relative";
+		tmp_img.style.transition = "opacity 0.25s";
+		maindiv.appendChild(tmp_img);
 
-	// console.log(this.array);
+		tmp_block.img = tmp_img; // adding to block
+
+		this.array.push(tmp_block); // adding block to array
+	}
 }
 
-
-
-// function swap_blocks(block1, block2) {
-// 	var tmp1 = block2.img_location;
-// 	var tmp2 = block2.img.current_loc;
-// 	var tmp3 = block2.selected;
-// 	block2.img_location = block1.img_location;
-// 	block2.current_loc = block1.current_loc;
-// 	block2.selected = block1.selected;
-// 	block1.img_location = tmp1;
-// 	block1.current_loc = tmp2;
-// 	block1.selected = tmp3;
-// 	block1.img.src = block1.img_location;
-// 	block2.img.src = block2.img_location;
-// }
-
 function randomize(array) {
-	arr = array.slice();
+	let arr = array.slice();
 	let size = array.length;
 	let curr_i = size-1;
-	// if()
-
-
 	while(curr_i != 0) {
 		let rand_i = Math.floor(Math.random()*curr_i);
 		curr_i-=1;
@@ -96,36 +76,32 @@ function init(level) {
 		remove_blocks();
 	}
 
-	if(solution_shown) {
+	if(solution_shown==true) {
 		remove_solution();
 		solution_shown = false;
 	}
 
-	if(level==1)
-		board = new Board(2, 1, 2);
-	else if(level==2) 
-		board = new Board(3, 1, 3);
-	else
-		board = new Board(10, 2, 5);
-	
-	// insert images
-	maindiv = document.querySelector("#block");
-	for(let i=0; i<board.array.length; i++) {
-		let tmp = document.createElement("img");
-		tmp.src = board.array[i].img_location;
-		tmp.width = 200;
-		tmp.classList = "imgs";
-		tmp.style.position = "relative";
-		tmp.style.transition = "opacity 1s";
-		maindiv.appendChild(tmp);
-		board.array[i].img = tmp;
-	}
+	if(level==1) board = new Board(2, 1, 2);
+	else if(level==2) board = new Board(3, 1, 3);
+	else board = new Board(10, 2, 5);
+
+
+	maindiv.addEventListener("click", (e)=> {
+		if(block_selected == false) {
+
+		} else {
+			let img = e.target;
+			if(img.classList == "imgs") {
+				img.addEventListener()
+			}
+		}
+	});
 
 	// initialise events for board
 	for(let i=0; i<board.array.length; i++) {
 		board.array[i].img.addEventListener("click", e => {
 			for(let j=0; j<board.array.length;j++) {
-				if(board.array[j].current_i == i){
+				if(board.array[j].current_loc == i){
 					block_click(j, e);
 					break;
 				}
@@ -138,15 +114,14 @@ function init(level) {
 
 function block_click(i, e) {
 	if(board.array[i].selected == false){
-		console.log("selected block");
+		console.log("selected block" , i);
 		block_select(i, e);
 	}
 	else{
-		console.log("deselect block");
+		console.log("deselect block", i);
 		block_deselect(i);
 	}
 }
-
 
 
 function block_select(i, e) {
@@ -155,17 +130,21 @@ function block_select(i, e) {
 	board.array[i].selected = true;
 	board.array[i].img.style.zIndex = 9999;
 	board.array[i].img.style.position = "absolute";
-	board.array[i].img.addEventListener("mousemove", block_move);
+	// board.array[i].img.addEventListener("mousemove", block_move);
+	maindiv.addEventListener("mousemove", block_move);
 }
 
 function block_deselect(i) {
 	board.array[i].selected = false;
 	board.array[i].img.style.opacity = 1;
 	board.array[i].img.style.zIndex = 1;
-	board.array[i].img.removeEventListener("mousemove", block_move);
+	maindiv.removeEventListener("mousemove", block_move);
 }
 
-function block_move(event) {
+function block_move(event)
+{
+
+	// console.log(event.target);
 
 	let mouse_x = event.pageX;
 	let mouse_y = event.pageY;
@@ -212,7 +191,6 @@ function update_location() {
 
 function add_solution() {
 	console.log("win");
-	let maindiv = document.querySelector("#block");
 	let dim = [maindiv.getBoundingClientRect()['width'], maindiv.getBoundingClientRect()['height']];
 
 	maindiv.style.display = "flex";
@@ -236,7 +214,7 @@ function add_solution() {
 
 function remove_solution() {
 	document.querySelector(".win-image").remove();
-	document.querySelector("#block").style.display = "block";
+	maindiv.style.display = "block";
 }
 
 function remove_blocks() {
