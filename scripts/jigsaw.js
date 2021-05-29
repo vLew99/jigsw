c50209d0bdeca8be485c354c89bb2c3aadd210c1
Vipul Chauhan <vip.chauhan99@gmail.com>
2021-05-29 15:13:57 +0530

    1. Converted approx all functions to pure functions. 2. Added block_selected and parent = maindiv in the board properties. 3. handled maindiv events

--- scripts/jigsaw.js
+++ scripts/jigsaw.js
@@ -4,8 +4,9 @@ var board;
 const maindiv = document.querySelector("#block");
 var block_selected = null;
 var level;
-var imgs_path;
+let imgs_path;
 var solution_shown;
+
 
 function Block(file_location, correct_loc, current_loc, img) {
 	this.img = img;
@@ -15,68 +16,61 @@ function Block(file_location, correct_loc, current_loc, img) {
 	this.selected = false;
 }
 
-function file_location_string(level) {
-	let file_location = "imgs/level" + level.toString() + "_";
-	file_location += Math.round(Math.random()*1).toString() + "_";
-	return file_location;
-}
-	
-function Board(size, row, column) {
-	this.array = new Array();
+function Board(size) {
 	this.size = size;
-	this.running = true;
 
 	// path
-	imgs_path = file_location_string(level);
+	imgs_path = getFileLocationStartPath(level); // move this somewhere other than here
+	let imgs_locs = getFileLocationsArray(imgs_path, size);
+	let rand_imgs_locs = imgs_locs.sort( () => Math.random() - 0.5); // randomize the file_locations_array and store in another
 
-	images = [];
-	for(let i=0; i < size; i++)
-		images.push(imgs_path + i.toString() + ".png");
-	rand_images = randomize(images);
-
-	// insert into array
-	let tmp_block;
-	let tmp_img;
-	for(let i=0; i<size;i++) {
-		// block
-		tmp_block = new Block(rand_images[i],parseInt(rand_images[i].charAt(14)),i,null);
-		
-		// img
-		tmp_img = document.createElement("img");
-		tmp_img.src = tmp_block.file_location; // getting file location from block
-		tmp_img.width = 200;
-		tmp_img.height = 200;
-		tmp_img.draggable = false;
-		tmp_img.classList = "imgs";
-		tmp_img.style.position = "relative";
-		tmp_img.style.transition = "opacity 0.25s";
-		maindiv.appendChild(tmp_img);
-
-		tmp_block.img = tmp_img; // adding to block
-
-		this.array.push(tmp_block); // adding block to array
-	}
+	this.array = new Array(this.size);
+	this.array = this.array.fill("");
+	this.array = this.array.map( (item, index) => {
+		return new Block(
+			rand_imgs_locs[index],
+			parseInt(rand_imgs_locs[index].charAt(14)),
+			index,
+			createImgElement(rand_imgs_locs[index])
+		);
+	});
+	this.block_selected = null;
+	this.parent = document.querySelector("#block");
 }
 
-function randomize(array) {
-	let arr = array.slice();
-	let size = array.length;
-	let curr_i = size-1;
-	while(curr_i != 0) {
-		let rand_i = Math.floor(Math.random()*curr_i);
-		curr_i-=1;
-		let temp = arr[curr_i];
-		arr[curr_i] = arr[rand_i];
-		arr[rand_i] = temp;
-	}
-	return arr;
-}
+// converted
+const getFileLocationStartPath = (level) => {
+	let flsp = "imgs/level" + level.toString() + "_";
+	flsp += Math.round(Math.random()*1).toString() + "_";
+	return flsp;
+};
 
+// converted
+const getFileLocationsArray = (imgs_path, array_size) => {
+	let fla = new Array(array_size);
+	fla = fla.fill("");
+	fla = fla.map( (item, index) => item += imgs_path + index.toString() + ".png");
+	return fla;
+};
+
+
+// converted
+const createImgElement = (file_location) => {
+	let img = document.createElement("img");
+	img.src = file_location;
+	img.width = img.height = 200;
+	img.draggable = false;
+	img.classList = "imgs";
+	img.style.position = "relative";
+	img.style.transition = "opacity 0.25s";
+	document.querySelector("#block").appendChild(img);
+	return img;
+};
 
 function init(level) {
 	block_selected = null;
 	if(board!=null) {
-		remove_blocks();
+		removeBoard(board);
 	}
 
 	if(solution_shown==true) {
@@ -84,117 +78,79 @@ function init(level) {
 		solution_shown = false;
 	}
 
-	if(level==1) board = new Board(2, 1, 2);
-	else if(level==2) board = new Board(3, 1, 3);
-	else board = new Board(10, 2, 5);
-
-}
-
-maindiv.addEventListener("mousedown", (e)=> {
-	let img = e.target;
-	if(img.classList == "imgs") {
-		if(block_selected == null) {
-			block_select(img);
-		}
-		else if(block_selected == img) {
-			block_deselect(img);
-		}
-	}
-});
-
-
-maindiv.addEventListener("mousemove", (e) => {
-	if(block_selected != null) {
-		let dim = maindiv.getBoundingClientRect();
-		let mouse_x = event.x;
-		let mouse_y = event.y;
-		let box_top = mouse_y - (block_selected.height/2) - dim['top'];
-		let box_left = mouse_x - (block_selected.width/2) - dim['left'];
-
-		if(mouse_y > dim['top'] && mouse_y < dim['bottom']) {
-			block_selected.style.top = box_top.toString() + "px";
-		}
-		if( mouse_x > dim['left'] && mouse_x < dim['right']) {
-			block_selected.style.left = box_left.toString() + "px";
-		}
-	}
-	console.log(1);
-});
-
-
-function block_select(img) {
-	img.style.opacity = 0.7;
-	img.style.zIndex = 9999;
-	img.style.position = "absolute";
-	block_selected = img;
-}
-
-function block_deselect(img) {
-	img.style.opacity = 1;
-	img.style.zIndex = 1;
-	block_selected = null;
-	update_location();
+	if(level==1) board = new Board(2);
+	else if(level==2) board = new Board(3);
+	else board = new Board(10);
 }
 
 
-function check_board_correct() {
-	let isCorrect = true;
-	for(let i=0; i<board.array.length; i++) {
-		if(i != board.array[i].correct_loc){
-			isCorrect = false;
-			break;
-		}
-	}
-	return isCorrect;
-}
+// converted
+const blockSelect = (img_to_select) => {
+	img_to_select.style.opacity = 0.7;
+	img_to_select.style.zIndex = 9999;
+	img_to_select.style.position = "absolute";
+	return img_to_select;
+};
 
-function update_location() {
-	board.array.sort((e1, e2)=> {
-		let e1_rect = e1.img.getBoundingClientRect();
-		let e2_rect = e2.img.getBoundingClientRect();
-		if(Math.abs(e1_rect['top'] - e2_rect['top']) < 200 )
-			return e1_rect['left'] - e2_rect['left'];
-		else
-			return e1_rect['top'] - e2_rect['top'];
-	});
-}
 
-function add_solution() {
-	console.log("win");
-	let dim = [maindiv.getBoundingClientRect()['width'], maindiv.getBoundingClientRect()['height']];
+// converted
+const blockDeselect = (img_to_deselect) => {
+	img_to_deselect.style.opacity = 1;
+	img_to_deselect.style.zIndex = 1;
+	return null;
+};
 
-	maindiv.style.display = "flex";
-	maindiv.style.flexDirection = "row";
-	maindiv.style.justifyContent = "space-around";
+// some additional checking needs to be done here
+const checkBoardCorrect = (board) => {
+	return board.array.every((item, index) => index == item.correct_loc);
+};
 
-	let tmp = document.createElement("img");
-	tmp.src = imgs_path + "image.jpg";
-	tmp.classList = "win-image";
-	tmp.style.position = "relative";
-	tmp.style.width = dim[0] + "px";
-	if(level==1)
-		tmp.style.height = dim[0]/2 + "px";
-	if(level==2)
-		tmp.style.height = dim[0]/3 + "px";
-	if(level==3)
-		tmp.style.height = (dim[0]/5)*2 + "px";	
-	// tmp.style.width = "auto";
-	maindiv.appendChild(tmp);
+
+// converted
+const blockSortFunction = (item1, item2) => {
+	let e1_rect = item1.img.getBoundingClientRect();
+	let e2_rect = item2.img.getBoundingClientRect();
+	if(Math.abs(e1_rect['top'] - e2_rect['top']) < 200 )
+		return e1_rect['left'] - e2_rect['left'];
+	else
+		return e1_rect['top'] - e2_rect['top']; 
+};
+
+
+// updates global board variable
+const updateBlockLocations = (board, blockSortFunction) => {
+	board.array.sort(blockSortFunction);
+};
+
+
+
+
+const addSolution = (imgs_path, parent) => {
+	let parent_rect = parent.getBoundingClientRect();
+	let solution = document.createElement("img");
+	solution.src = imgs_path + "image.jpg";
+	solution.classList = "win-image";
+	solution.style.position = "absolute";
+	solution.style.top = "50%";
+	solution.style.transform = "translateY(-50%)";
+	solution.style.maxWidth = parent_rect['width'] + "px";
+	solution.style.height = "auto";
+	parent.appendChild(solution);
+};
+
+
+const removeSolution = (solution) => {
+	solution.remove();
 }
 
 function remove_solution() {
 	document.querySelector(".win-image").remove();
-	maindiv.style.display = "block";
 }
 
-function remove_blocks() {
-	for(let i=0; i<board.array.length; i++) {
-		board.array[i].img.remove();
-	}
-	block_selected = null;
+const removeBoard = (board) => {
+	board.array.map( item => item.img.remove() );
 	board = null;
-}
-
+};
 
 
 
@@ -211,27 +167,54 @@ function sleep(ms) {
 
 
 
+// EVENT FUNCTIONS
+const maindivMouseClick = (event, board, blockSelect, blockDeselect, updateBlockLocations) => {
+	let img = event.target;
+	if(img.classList == "imgs") {
+		if(board.block_selected == null) {
+			board.block_selected = blockSelect(img);
+		}
+		else if(board.block_selected == img) {
+			board.block_selected = blockDeselect(img);
+			updateBlockLocations(board, );
+		}
+	}
+};
+
+
+
+const maindivMouseMove = (event, board) => {
+	if(board.block_selected != null) {
+		let dim = board.parent.getBoundingClientRect();
+		let mouse_x = event.x;
+		let mouse_y = event.y;
+		let box_top = mouse_y - (board.block_selected.height/2) - dim['top'];
+		let box_left = mouse_x - (board.block_selected.width/2) - dim['left'];
+
+		if(mouse_y > dim['top'] && mouse_y < dim['bottom']) {
+			board.block_selected.style.top = box_top.toString() + "px";
+		}
+		if( mouse_x > dim['left'] && mouse_x < dim['right']) {
+			board.block_selected.style.left = box_left.toString() + "px";
+		}
+	}
+};
+
 
 
 // events
-document.querySelector("#level1").addEventListener("click", e => {
-	start(1);
-});
-
-document.querySelector("#level2").addEventListener("click", e => {
-	start(2);
-});
-
-document.querySelector("#level3").addEventListener("click", e => {
-	start(3);
-});
+document.querySelector("#block").addEventListener("click", event => maindivMouseClick(event, board, blockSelect, blockDeselect));
+document.querySelector("#block").addEventListener("mousemove", event => maindivMouseMove(event, board));
+document.querySelector("#level1").addEventListener("click", event => start(1));
+document.querySelector("#level2").addEventListener("click", e => start(2));
+document.querySelector("#level3").addEventListener("click", e => start(3));
 
 document.querySelector("#check").addEventListener("click", e => {
 	if(board!=null) {
-		update_location();
-		if(check_board_correct() && !solution_shown) {
-			remove_blocks();
-			add_solution();
+		updateBlockLocations(board, blockSortFunction);
+		if(checkBoardCorrect(board) && !solution_shown) {
+			removeBoard(board);
+			addSolution(imgs_path, maindiv);
 			solution_shown = true;
 		}
 		else {
@@ -244,7 +227,6 @@ document.querySelector("#check").addEventListener("click", e => {
 		}
 	}
 })
-
 
 
 // //////////////////////////////////// RUNNNNN
